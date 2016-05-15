aa203a2a77908cef0ea3eec99a807023dcbf29b3
Hanani <hanani@xentralmethods.com>
2016-05-15 11:55:02 +0800

    Removed about fragment

--- app/src/main/java/com/android/findit/MainActivity.java
+++ app/src/main/java/com/android/findit/MainActivity.java
@@ -1,8 +1,6 @@
 package com.android.findit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,7 +8,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -99,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show total pages.
-            return 3;
+            return 2;
         }
 
         @Override
