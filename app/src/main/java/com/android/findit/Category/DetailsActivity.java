4592c9e61cb519627b867ee8c7ad4828e16d72ab
Hanani <hanani@xentralmethods.com>
2016-05-15 11:55:52 +0800

    Added details activity

---
+++ app/src/main/java/com/android/findit/Category/DetailsActivity.java
@@ -0,0 +1,17 @@
+package com.android.findit.Category;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.findit.R;
+
+/**
+ * Created by Hanani on 12/5/2016.
+ */
+public class DetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_activity);
+    }
+}
