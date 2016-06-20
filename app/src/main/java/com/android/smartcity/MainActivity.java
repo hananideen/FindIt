00407a9e8ec86be95c16fdf8f57a0c18f45af52a
Hanani <hanani@xentralmethods.com>
2016-06-20 15:42:19 +0800

    Create nav drawer

--- app/src/main/java/com/android/smartcity/MainActivity.java
+++ app/src/main/java/com/android/smartcity/MainActivity.java
@@ -5,15 +5,21 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
@@ -22,8 +28,9 @@ import com.android.smartcity.Helper.Constant;
 import com.android.smartcity.Services.Category.CategoryFragment;
 import com.android.smartcity.Services.MapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawer;
     private AppBarLayout appBarLayout;
     private CoordinatorLayout view;
 
@@ -33,9 +40,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
         view = (CoordinatorLayout) findViewById(R.id.main_content);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         setupView();
 
         Window window = getWindow();
@@ -48,25 +63,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -104,6 +106,24 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         if (tabLayout!=null)
             tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile) {
+            Snackbar.make(drawer, "User Profile Coming Soon", Snackbar.LENGTH_LONG).show();
+        } else if (id == R.id.nav_settings) {
+            Snackbar.make(drawer, "Settings Coming Soon", Snackbar.LENGTH_LONG).show();
+        } else if (id == R.id.nav_about) {
+            Snackbar.make(drawer, "About Us Coming Soon", Snackbar.LENGTH_LONG).show();
+        } else if (id == R.id.nav_feedback) {
+            Snackbar.make(drawer, "Feedback Coming Soon", Snackbar.LENGTH_LONG).show();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private class SectionsPagerAdapter extends FragmentPagerAdapter {
