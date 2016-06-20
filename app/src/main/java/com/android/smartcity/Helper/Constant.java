b5956e6751f70226ada39608fa76042dcf7e0110
Hanani <hanani@xentralmethods.com>
2016-06-20 14:50:27 +0800

    Scrollable tab

--- app/src/main/java/com/android/smartcity/Helper/Constant.java
+++ app/src/main/java/com/android/smartcity/Helper/Constant.java
@@ -14,7 +14,7 @@ public class Constant {
     /******FRAGMENT******/
     public static final String ARG_SECTION_NUMBER = "section_number";
     public static final String NAME_MAP = "Map View";
-    public static final String NAME_CATEGORIES = "Categories";
+    public static final String NAME_CATEGORIES = "Services";
     public static final String NAME_OPPORTUNITIES = "Opportunities";
     public static final String NAME_ACCOMMODATION = "Accommodation";
     public static final String NAME_EVENTS = "Contact & Events";
